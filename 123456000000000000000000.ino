48f5f314595c1ca260c30763a53a0934cd6b6b07
IoTopia.io <iotopia.io@gmail.com>
2017-09-30 02:23:03 +0100

    Update via IoTopia.io

---
+++ 123456000000000000000000.ino
@@ -0,0 +1,19 @@
+0000000000000000000000000111111111111111111111111
+long baudRate = 115200;
+const char* strBoard = "UnoI_Board_4";
+
+void output(String topic, String msg){
+    Serial.println(topic+" | "+msg);
+}
+
+void initSerial(){
+    Serial.begin(baudRate);
+    output("Board",String(strBoard));
+}
+
+void setup() {
+    initSerial();
+}
+
+void loop() {
+}
